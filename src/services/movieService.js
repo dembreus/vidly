6115cf356a5e040d76e7747f387c983321090622
Dillon <dillon.embreus@gmail.com>
2019-02-25 11:52:42 -0700

    Deleted fake services. Refactored movieService

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -3,24 +3,28 @@ import { apiUrl } from "../config.json";
 
 const url = `${apiUrl}/movies`;
 
+function movieUrl(id) {
+  return `${url}/${id}`;
+}
+
 export function getMovies() {
   return http.get(url);
 }
 
 export function getMovie(movieId) {
-  return http.get(`${url}/${movieId}`);
+  return http.get(movieUrl(movieId));
 }
 
 export function saveMovie(movie) {
   if (movie._id) {
     const body = { ...movie };
     delete body._id;
-    return http.put(`${url}/${movie._id}`, body);
+    return http.put(movieUrl(movie._id), body);
   }
 
   return http.post(url, movie);
 }
 
 export function deleteMovie(movieId) {
-  return http.delete(`${url}/${movieId}`);
+  return http.delete(movieUrl(movieId));
 }
